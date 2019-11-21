1c9725005c0c2d8806a3f0269e97a018e57e0fb0
WhismicalWallaby <rpsp5jwetb4@opayq.com>
2019-11-21 11:31:24 +0800

    Cleaned up code

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,10 @@
-for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
-        i = "FizzBuzz"
-    elif i % 3 == 0:
-        i = "Fizz"
-    elif i % 5 == 0:
-        i = "Buzz"
-    print(i)
+def fizzbuzz(n):
+    for i in range(n):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+     elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
